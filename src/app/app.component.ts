5279ef33e3f2821da22b0b9be36ba38d906fc289
Valeriya Stange <valeriya.stange@gmail.com>
2025-04-11 16:15:55 +0200

    Added header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContactsListComponent } from "./components/contacts-list/contacts-list.component";
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContactsListComponent, ContactsListComponent],
+  imports: [RouterOutlet, ContactsListComponent, ContactsListComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
