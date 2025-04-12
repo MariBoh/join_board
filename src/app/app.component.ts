bb4e7c413da033455e0b5ed77798a2dc48a28fc8
Taher-IT <tah.abd.megid@gmail.com>
2025-04-12 11:00:47 +0200

    routes für home, header und footer erstellt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ContactsListComponent } from "./components/contacts-list/contacts-list.component";
 import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from "./shared/footer/footer.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContactsListComponent, ContactsListComponent, HeaderComponent],
+  imports: [CommonModule , RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
   title = 'join_board';
+  
 }
