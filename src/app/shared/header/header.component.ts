c5d81600c5c06e2d3dcdd7d24d7dd174029ed993
Valeriya Stange <valeriya.stange@gmail.com>
2025-04-13 16:27:55 +0200

    header  1.1.2 & navbar 1.1.2

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { CommonModule } from '@angular/common'; // ⬅️ das brauchst du
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule], // ⬅️ hier hinzufügen
+  imports: [CommonModule], 
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
