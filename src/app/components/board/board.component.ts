b3f56bb5b228004d50f57cacb1d12e350092f11e
Taher-IT <tah.abd.megid@gmail.com>
2025-04-30 08:38:15 +0200

    Board component design elements implemented + responsive v0.1

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -11,7 +11,7 @@ import { FAKE_TASKS, FAKE_CONTACTS } from '../../models/fake-data.model';
   standalone: true,
   imports: [CdkDropListGroup, CdkDropList, CdkDrag, CommonModule],
   templateUrl: './board.component.html',
-  styleUrl: './board.component.scss'
+  styleUrls: ['./board.component.scss', './board.responsive.scss']
 })
 
 export class BoardComponent {
@@ -19,70 +19,6 @@ export class BoardComponent {
   constructor(public firebaseService: FirebaseService) {
 
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-  // inProgress = ['Develop feature', 'Write tests', 'Fix bug', 'Drink coffee'];
-  // awaitFeedback = ['Review bugfix', 'Test Formatting', 'Check spelling', 'Check grammar'];
-  // done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
-
-  // drop(event: CdkDragDrop<string[]>) {
-  //   if (event.previousContainer === event.container) {
-  //     moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-  //   } else {
-  //     transferArrayItem(
-  //       event.previousContainer.data,
-  //       event.container.data,
-  //       event.previousIndex,
-  //       event.currentIndex,
-  //     );
-  //   }
-  // }
-
-  // getContactNameById(id: string): string {
-  //   let contact = this.firebaseService.contacts.find(c => c.id === id);
-  //   return contact ? contact.name : 'Unbekannt';
-  // }
-
-
-
-
-  // Fake Data for testing purposes
 
   columns = [
     { title: 'To do', tasks: [...FAKE_TASKS] },
@@ -116,4 +52,5 @@ export class BoardComponent {
     const contact = FAKE_CONTACTS.find(c => c.fakeId === fakeId);
     return contact ? contact.fakeName.split(' ').map(n => n[0]).join('') : '?';
   }
+
 }
